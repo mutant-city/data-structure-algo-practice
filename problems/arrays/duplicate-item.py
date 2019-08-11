8da4f0dd14abbd74305364b30e32c9fa849404cd
Nick Kiermaier <nickkiermaier@gmail.com>
2019-08-11 15:53:34 -0500

    upd

---
+++ problems/arrays/duplicate-item.py
@@ -0,0 +1,4 @@
+# Given an array of integers, check if an element exists twice in the list, 
+# using an efficient data structure and algorithm. Also compute Time and Space complexity.  
+
+
---
+++ problems/arrays/subarray-largest-sum.py
@@ -0,0 +1,1 @@
+# find the subarray with the largest sum.  
