417cc19a16520f5af60676fb8a44075226d238d6
mutant admin <mutant@mutant.city>
2020-08-09 08:59:25 -0500

    upd

--- preparation/sorting/quick.py
+++ preparation/sorting/quick.py
@@ -1,26 +1,32 @@
-def partition(in_array, partition_value):
+def partition(in_array, partition_value, left_index, right_index):
 
-    insertion_index = 0
+    while left_index < right_index:
+        if in_array[left_index] > partition_value and in_array[right_index] < partition_value:
+            tmp = in_array[left_index]
+            in_array[left_index] = in_array[right_index]
+            in_array[right_index] = tmp
 
-    for swap_index in range(0, len(in_array)):
-        if in_array[swap_index] < partition_value:
-            # swap
-            tmp = in_array[insertion_index]
-            in_array[insertion_index] = in_array[swap_index]
-            in_array[swap_index] = tmp
+        if in_array[left_index] < partition_value:
+            left_index += 1
 
-            # increment index
-            insertion_index += 1
+        if in_array[right_index] > partition_value:
+            right_index -=1
 
 
-
+# mid value of begnning, middle, and end
 def find_pivot(in_array):
-    length = len(in_array)
-    first = in_array[0]
-    middle = in_array[(length-1)/2]
-    last = in_array[length-1]
-    return max([first, middle, last])
+    possibles = [in_array[0], in_array[(len(in_array)-1)//2], in_array[len(in_array)-1]]
+    possibles.remove(max(possibles))
+    possibles.remove(min(possibles))
+    return possibles[0]
 
+
+def quick_recurse(in_array, left_index, right_index):
+    pivot = find_pivot(in_array)
+    partition(in_array, pivot, left_index, right_index)
+    split_index = (left_index - right_index) / 2
+    quick_recurse(in_array,left_index, split_index)
+    quick_recurse(in_array, split_index, right_index)
 
 def quick(in_array):
-    pivot = find_pivot(in_array)
+    quick_recurse(in_array, 0, len(in_array) - 1)
